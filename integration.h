fe4779374a5188b920733bd534997f3b64a07d34
jowi-ka <jowi.dru@gmail.com>
2026-05-14 14:05:52 +0200

    Add integration.h header with function prototypes

--- integration.h
+++ integration.h
@@ -1,1 +1,9 @@
-#pragma once
+#ifndef INTEGRATION_H
+#define INTEGRATION_H
+
+double obliczMetodaProstokatow(double poczatekPrzedzialu, double koniecPrzedzialu, int liczbaPodprzedzialow);
+double obliczMetodaTrapezow(double poczatekPrzedzialu, double koniecPrzedzialu, int liczbaPodprzedzialow);
+double obliczMetodaMonteCarlo(double poczatekPrzedzialu, double koniecPrzedzialu, int liczbaPodprzedzialow);
+
+
+#endif
